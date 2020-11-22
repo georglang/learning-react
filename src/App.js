be0a7f1ce6be0381228a908a2d3bf55cbe883e55
Georg Lang <mail@georglang.com>
2020-11-22 15:40:26 +0100

    appied React Hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Georg', age: 30 }
     ]
-  }
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         { name: 'Hans', age: 55 }
       ]
     })
   }
 
-  render() {
-    return (
-      <div className="App" >
-        <h1>Hi I´m a react app</h1>
+  return (
+    <div className="App" >
+      <h1>Hi I´m a react app</h1>
 
-        <button onClick={this.switchNameHandler}>
-          Switch Name Handler
+      <button onClick={switchNameHandler}>
+        Switch Name Handler
         </button>
 
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-      </div>
-    );
-  }
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+    </div>
+  );
 }
 
 export default App;
+
