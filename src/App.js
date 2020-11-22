31775559ac9d7b6fc6c5ece920f85a4ee828544d
Georg Lang <mail@georglang.com>
2020-11-22 15:05:24 +0100

    props and state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
+import { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi I´m a react app</h1>
-      <Person/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Georg', age: 30 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    this.setState({
+      persons: [
+        { name: 'Hans', age: 55 }
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <h1>Hi I´m a react app</h1>
+
+        <button onClick={this.switchNameHandler}>
+          Switch Name Handler
+        </button>
+
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const person = () => {
-  return <h1>I´m a Person</h1>
+const person = (props) => {
+  return (
+    <div>
+      <p>I´m {props.name} and I´m {props.age} years old</p>
+    </div>
+  );
 }
 
 export default person;
